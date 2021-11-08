a1b81d5b428f27704abeaf8cbc49a6e77df482d4
kumarmanas9931 <86017812+kumarmanas9931@users.noreply.github.com>
2021-11-08 22:28:56 +0530

    Add files via upload

---
+++ DATASTRUCTURELAB3/src/PROBLEM1/BalanceCheck.java
@@ -0,0 +1,42 @@
+package PROBLEM1;
+import java.util.Stack;
+
+public class BalanceCheck {
+	public boolean balanceChecking(String str) {
+		Stack<Character> st = new Stack<>();
+		char[] arr = str.toCharArray();
+		char x;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == '(' || arr[i] == '{' || arr[i] == '[') {
+				st.push(arr[i]);
+				continue;
+			}
+			if (st.isEmpty())
+				return false;
+
+			switch (arr[i]) {
+			case ')':
+				x = st.pop();
+				if (x == '{' || x == '[')
+					return false;
+				break;
+
+			case '}':
+				x = st.pop();
+				if (x == '(' || x == '[')
+					return false;
+				break;
+
+			case ']':
+				x = st.pop();
+				if (x == '{' || x == '(')
+					return false;
+				break;
+			}
+		}
+		return st.isEmpty();
+	}
+}
+
+
+
---
+++ DATASTRUCTURELAB3/src/PROBLEM1/Driver.java
@@ -0,0 +1,22 @@
+package PROBLEM1;
+import java.util.Scanner;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean flag = true;
+		Scanner sc = new Scanner(System.in);
+		BalanceCheck bc = new BalanceCheck();
+		System.out.println("Enter the expression to check its balancing");
+		String expr = sc.next();
+		flag = bc.balanceChecking(expr);
+		if (flag == false)
+			System.out.println("Given expression is not balanced");
+		else
+			System.out.println("Given expression is balanced");
+		sc.close();
+
+	}
+
+}
